3ed754287cbc3d4af88f3194ef58c130d52e0634
Kobe <dev.skyachieve91@gmail.com>
2023-11-11 18:23:28 +0900

    :sparkles: Feat: init

---
+++ Example/Pods/Target Support Files/Pods-UIKobeKit_Example/Pods-UIKobeKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UIKobeKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_UIKobeKit_Example
+@end
---
+++ Example/Pods/Target Support Files/UIKobeKit/UIKobeKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_UIKobeKit : NSObject
+@end
+@implementation PodsDummy_UIKobeKit
+@end
