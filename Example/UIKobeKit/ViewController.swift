6ea757d90b99628042e65c70d11b53488443152c
Kobe <dev.skyachieve91@gmail.com>
2023-11-11 18:07:30 +0900

    Initial commit

---
+++ Example/UIKobeKit/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIKobeKit
+//
+//  Created by devKobe24 on 11/11/2023.
+//  Copyright (c) 2023 devKobe24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
