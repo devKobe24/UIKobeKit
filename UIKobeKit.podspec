bd1fbef41b6e2be8c05538db1054331ed656a7e1
Kobe <dev.skyachieve91@gmail.com>
2023-11-11 19:23:47 +0900

    :bookmark: Feat: UIKobeKit pod spec lint passed validation.

--- UIKobeKit.podspec
+++ UIKobeKit.podspec
@@ -4,6 +4,7 @@ Pod::Spec.new do |s|
   s.summary          = 'You can create a color by using HEX values instead of RGB values.'
   s.description      = 'Unlike the cumbersome process of inputting RGB values one by one to create color values, with UIKobeKit, you can easily and quickly create your own unique and special colors using HEX values. Now, you can effortlessly and swiftly generate colors with HEX values, without the hassle of dealing with RGB values.'
   s.homepage         = 'https://github.com/devKobe24/UIKobeKit'
+  s.swift_version = '4.2'
   # s.screenshots     = 'www.example.com/screenshots_1', 'www.example.com/screenshots_2'
   s.license          = { :type => 'MIT', :file => 'LICENSE' }
   s.author           = { 'devKobe24' => 'dev.skyachieve91@gmail.com' }
---
+++ UIKobeKit/Classes/UIKobeKit.swift
@@ -0,0 +1,71 @@
+//
+// UIKobeKit.swift
+// UIKobeKit
+//
+// Created by Kobe on 11/11/2023
+// Copyright (c) 2023 Kobe All rights reserved.
+
+import UIKit
+
+extension UIColor {
+    
+    public convenience init(hexCode: String, alpha: CGFloat) {
+        
+        var hexFormatted: String = hexCode.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines).uppercased()
+        
+        if hexFormatted.hasPrefix(Prefix.crossHatch.value) {
+            hexFormatted = String(hexFormatted.dropFirst())
+        }
+        
+        assert(hexFormatted.count == 6, UIKobeKitError.invalidHEXCode.localizedDescription)
+        
+        var rgbColorValue: UInt64 = 0
+        Scanner(string: hexFormatted).scanHexInt64(&rgbColorValue)
+        
+        self.init(
+            red: CGFloat((rgbColorValue & ColorCode.firstBlack.value) >> 16) / 255.0,
+            green: CGFloat((rgbColorValue & ColorCode.secondBlack.value) >> 8) / 255.0,
+            blue: CGFloat(rgbColorValue & ColorCode.blue.value) / 255.0,
+            alpha: alpha
+        )
+    }
+}
+
+enum Prefix {
+    case crossHatch
+    
+    var value: String {
+        switch self {
+        case .crossHatch:
+            return "#"
+        }
+    }
+}
+
+enum UIKobeKitError: LocalizedError {
+    case invalidHEXCode
+    
+    var errorDescription: String? {
+        switch self {
+        case .invalidHEXCode:
+            "Use of invalid HEX CODE value."
+        }
+    }
+}
+
+enum ColorCode {
+    case firstBlack
+    case secondBlack
+    case blue
+    
+    var value: UInt64 {
+        switch self {
+        case .firstBlack:
+            return 0xFF0000
+        case .secondBlack:
+            return 0x00FF00
+        case .blue:
+            return 0x0000FF
+        }
+    }
+}
